78b3c6af6279584f27c1a76574815af464415014
Mujahidh Haseem <mujahidhaseem@gmail.com>
2020-04-03 11:53:38 +0530

    Delete registration.php

--- registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Eight25media_OrdersModification',
-    __DIR__
-);
